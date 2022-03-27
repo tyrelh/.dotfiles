9529043a0779e78f2087684e3b381fabdd324230
tyrelh <tyrel.hiebert@gmail.com>
2022-03-27 12:54:42 -0700

    update install script WIP

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
+# zsh / ohmyzsh
+apt install -y zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# installing ohmyzsh will already back up the existing .zshrc
 rm ~/.zshrc
 ln -s .zshrc ~/.zshrc
-mkdir ~/.config
-mkdir ~/.config/nvim
-ln -s .config/nvim/init.vim ~/.config/nvim/init.vim
-ls -s .config/nvim/ultisnips ~/.config/nvim/ultisnips
+
+# powerline10k
+
+# neovim
+# mkdir ~/.config
+# mkdir ~/.config/nvim
+# ln -s .config/nvim/init.vim ~/.config/nvim/init.vim
+# ls -s .config/nvim/ultisnips ~/.config/nvim/ultisnips
