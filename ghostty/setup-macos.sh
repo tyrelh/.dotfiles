8b4617b7c4c5aa4050d667ec0ca5328dde388c7e
Tyrel Delaney <tyrel.hiebert@gmail.com>
2025-01-08 08:58:48 -0800

    improve script to handle no existing ghostty config directory

--- ghostty/setup-macos.sh
+++ ghostty/setup-macos.sh
@@ -1,2 +1,7 @@
-mv ~/.config/ghostty/config ~/.config/ghostty/config.bak
-ln config ~/.config/ghostty/config
+#! /usr/bin/env bash
+if [ -d "~/.config/ghostty" ]; then
+    mv ~/.config/ghostty/config ~/.config/ghostty/config.bak
+else
+    mkdir ~/.config/ghostty
+fi
+ln -s ~/.dotfiles/ghostty/config ~/.config/ghostty/config
