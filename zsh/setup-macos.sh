358b6c6b642b309c27b3c62ecfa8102d5fe454d2
Tyrel Delaney <tyrel.hiebert@gmail.com>
2025-01-31 11:55:33 -0800

    some additional setup for zsh config

--- zsh/setup-macos.sh
+++ zsh/setup-macos.sh
@@ -1,6 +1,9 @@
 mv ~/.zshrc ~/.zshrc.bak
 ln .zshrc ~/.zshrc
 
+mv ~/.zprofile ~/.zprofile.bak
+ln .zprofile ~/.zprofile
+
 brew install starship
 mv ~/.config/starship.toml ~/.config/starship.toml.bak
 ln starship.toml ~/.config/starship.toml
